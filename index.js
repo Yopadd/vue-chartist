2074a59b17b4f1666b502d6e0feb9b62494ad8cc
Yopadd <yoann.bourdex@gmail.com>
2015-12-14 10:54:47 +0100

    add babel compiler before publish

--- index.js
+++ index.js
@@ -1,57 +1,84 @@
-exports.install = function (Vue, custmOptions={}) {
-    const defaultOptions = {
+'use strict';
+
+exports.install = function (Vue) {
+    var custmOptions = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    var defaultOptions = {
         messageNoData: ''
-    }
-    const options = Object.assign({}, defaultOptions, custmOptions)
+    };
+    var options = Object.assign({}, defaultOptions, custmOptions);
 
     Vue.component('chartist', {
-        template:'<div :id="idChart" :class="[ratio, noData]">{{message}}</div>',
-        ready: function () {
-            this.draw()
+        template: '<div :id="idChart" :class="[ratio, noData]">{{message}}</div>',
+        ready: function ready() {
+            this.draw();
         },
         props: {
-            idChart: { type: String, required: true},
-            ratio: { type: String},
-            data: { type: Object},
-            options: { type: Object},
-            type: { type: String, required: true, validator: function (val) { return val === 'Pie' || val === 'Line' || val === 'Bar' } },
+            idChart: { type: String, required: true },
+            ratio: { type: String },
+            data: { type: Object },
+            options: { type: Object },
+            type: { type: String, required: true, validator: function validator(val) {
+                    return val === 'Pie' || val === 'Line' || val === 'Bar';
+                } },
             eventHanders: { type: Array },
             responsiveOptions: { type: Object }
         },
-        data: function () {
+        data: function data() {
             return {
                 error: { onError: false, message: "" },
                 noData: '',
                 message: ''
-            }
+            };
         },
         methods: {
-            draw: function () {
+            draw: function draw() {
                 if (this.data) {
                     //data empty
-                    if (this.data.series.length < 1 || (this.type !== 'Pie' && this.data.labels.length < 1)) {
-                        new Chartist[this.type]('#'+this.idChart, this.data, this.options, this.responsiveOptions) //clear the potential old chart
-                        this.setNoData()
-                    //data ok
+                    if (this.data.series.length < 1 || this.type !== 'Pie' && this.data.labels.length < 1) {
+                        new Chartist[this.type]('#' + this.idChart, this.data, this.options, this.responsiveOptions); //clear the potential old chart
+                        this.setNoData();
+                        //data ok
                     } else {
-                        this.noData = '' //remove class ct-nodata
-                        this.message = '' //remove message no data
-                        if (this.error.onError) this.error = { onError: false, message: "" } //clear error
-                        const chart = new Chartist[this.type]('#'+this.idChart, this.data, this.options, this.responsiveOptions)
-                        if (this.eventHanders) {
-                            for (let el of this.eventHanders) {
-                                chart.on(el.event, el.fn)
+                            this.noData = ''; //remove class ct-nodata
+                            this.message = ''; //remove message no data
+                            if (this.error.onError) this.error = { onError: false, message: "" }; //clear error
+                            var chart = new Chartist[this.type]('#' + this.idChart, this.data, this.options, this.responsiveOptions);
+                            if (this.eventHanders) {
+                                var _iteratorNormalCompletion = true;
+                                var _didIteratorError = false;
+                                var _iteratorError = undefined;
+
+                                try {
+                                    for (var _iterator = this.eventHanders[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                                        var el = _step.value;
+
+                                        chart.on(el.event, el.fn);
+                                    }
+                                } catch (err) {
+                                    _didIteratorError = true;
+                                    _iteratorError = err;
+                                } finally {
+                                    try {
+                                        if (!_iteratorNormalCompletion && _iterator.return) {
+                                            _iterator.return();
+                                        }
+                                    } finally {
+                                        if (_didIteratorError) {
+                                            throw _iteratorError;
+                                        }
+                                    }
+                                }
                             }
                         }
-                    }
                 } else {
-                    this.setNoData()
+                    this.setNoData();
                 }
             },
-            setNoData: function () {
-                this.error = { onError: true, message: options.messageNoData }
-                this.noData = 'ct-nodata'
-                this.message = this.error.message
+            setNoData: function setNoData() {
+                this.error = { onError: true, message: options.messageNoData };
+                this.noData = 'ct-nodata';
+                this.message = this.error.message;
             }
         },
         watch: {
@@ -64,5 +91,5 @@ exports.install = function (Vue, custmOptions={}) {
             },
             'type': 'draw'
         }
-    })
-}
+    });
+};
