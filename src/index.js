c68b619c0b76fd3fc0f47b71992c15393581071d
yopadd <yoann.bourdex@gmail.com>
2016-05-18 16:23:54 +0200

    add dependencies chartist

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ exports.install = function (Vue, options={}) {
         },
         data() {
             return {
-                Chartist: typeof window.Chartist === 'undefined' ? require('chartist') : window.Chartist,
+                Chartist: require('chartist'),
                 error: { onError: false, message: '' },
                 noData: '',
                 message: ''
